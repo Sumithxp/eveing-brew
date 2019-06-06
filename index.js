01227c8de88b7decfb07ded9693fc8b6a0252e40
Sumith Fernando <srn@tiqri.com>
2019-06-06 12:13:30 +0530

    add api controllers

--- index.js
+++ index.js
@@ -8,6 +8,20 @@ app.get('/', function(request, response) {
   response.send('Hello World!')
 })
 
+
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
+
+var productController = require('./Controller/ProductController')();
+app.use("/api/products", productController);
+
+var appointmentController = require('./Controller/AppointmentController')();
+app.use("/api/appointments", appointmentController);
+
+
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
